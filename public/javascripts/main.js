7716eade4ca89d5b5e426d7f609a60fc65cdd7dd
n-david <ngdavid.93@gmail.com>
2017-07-05 00:30:26 -0700

    finished replace function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,7 @@ $(function() {
 	$('.submit').click(function(e) {
 		e.preventDefault();
 		var text = $('.text').val();
-		text = text.replace(/( \/ )|(\.)|( #)|( )/g, '_');
+		text = text.replace(/( [^a-zA-Z0-9-] )|( [^a-zA-Z0-9-])|([^a-zA-Z0-9-] )/g, '_').replace(/[^a-zA-Z0-9-]/g,'_');
 		console.log(text);
 		$('#result').text(text);
 	});
