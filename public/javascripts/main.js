4b1acb5ab54658e1828400c5c135f3013af3c2f3
n-david <ngdavid.93@gmail.com>
2017-07-05 00:50:32 -0700

    added copy function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,24 @@
 $(function() {
+
+	function copyToClipboard(element) {
+		var $temp = $('<input>');
+		$('body').append($temp);
+		$temp.val($(element).text()).select();
+		document.execCommand('copy');
+		$temp.remove();
+	}
+
 	$('.submit').click(function(e) {
 		e.preventDefault();
 		var text = $('.text').val();
-		text = text.replace(/( [^a-zA-Z0-9-] )|( [^a-zA-Z0-9-])|([^a-zA-Z0-9-] )/g, '_').replace(/[^a-zA-Z0-9-]/g,'_');
-		console.log(text);
+		text = text.replace(/( [^a-zA-Z0-9-] )|( [^a-zA-Z0-9-])|([^a-zA-Z0-9-] )/g,
+				'_').replace(/[^a-zA-Z0-9-]/g, '_');
 		$('#result').text(text);
+		$('.copy').addClass('display');
+	});
+
+	$('.copy').click(function(e) {
+		e.preventDefault();
+		copyToClipboard($('#result'));
 	});
 });
