5098bc5d6560ba331a368e5ef2daa12f84713784
n-david <ngdavid.93@gmail.com>
2017-07-10 20:36:18 -0700

    added delete apostrophes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,7 +13,7 @@ $(function() {
 		var $text = $('.text');
 		var input = $text.val();
 		$text.val('');
-		input = input.replace(/( [^a-zA-Z0-9-] )|( [^a-zA-Z0-9-])|([^a-zA-Z0-9-] )/g,
+		input = input.replace(/'/g, '').replace(/( [^a-zA-Z0-9-] )|( [^a-zA-Z0-9-])|([^a-zA-Z0-9-] )/g,
 				'_').replace(/[^a-zA-Z0-9-]/g, '_');
 		$('#result').text(input.toLowerCase());
 		$('.copy').addClass('display');
